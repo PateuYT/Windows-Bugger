3fdafc11346e3011706fe2fca1787bf2fc436566
P4TEU <168577505+PateuYT@users.noreply.github.com>
2026-05-22 06:45:59 +0100

    Update do not open.bat

--- do not open.bat
+++ do not open.bat
@@ -3,12 +3,10 @@ setlocal
 
 set "desktop=%USERPROFILE%\Desktop"
 
-for /L %%i in (1,1,1000) do echo Dumb you've been crashed>>"%desktop%\\Youvebeenhacked1.txt"
-echo Dumb you've been crashed > "%desktop%\Youvebeenhacked2.txt"
-echo Dumb you've been crashed > "%desktop%\Youvebeenhacked3.txt"
-echo Dumb you've been crashed > "%desktop%\Youvebeenhacked4.txt"
-echo Dumb you've been crashed > "%desktop%\Youvebeenhacked5.txt"
-echo Dumb you've been crashed > "%desktop%\Youvebeenhacked6.txt"
+@echo off
+for /L %%i in (1,1,1000) do (
+    echo Dumb you've been crashed > "%desktop%\Youvebeenhacked%%i.txt"
+)
 
 echo.
 echo You've been succesfully fucked by P4TEU!
