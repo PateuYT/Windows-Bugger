57207c6bbaed34076d4e81ede1a7ee36790a337a
P4TEU <168577505+PateuYT@users.noreply.github.com>
2026-05-22 06:45:14 +0100

    Update do not open.bat

--- do not open.bat
+++ do not open.bat
@@ -1,16 +1,14 @@
 @echo off
 setlocal
 
-:: Get Desktop path
 set "desktop=%USERPROFILE%\Desktop"
 
-:: Create the files
-for /L %%i in (1,1,1000) do echo Niga you've been hacked>>"%desktop%\\Youvebeenhacked1.txt"
-echo Niga you've been hacked > "%desktop%\Youvebeenhacked2.txt"
-echo Niga you've been hacked > "%desktop%\Youvebeenhacked3.txt"
-echo Niga you've been hacked > "%desktop%\Youvebeenhacked4.txt"
-echo Niga you've been hacked > "%desktop%\Youvebeenhacked5.txt"
-echo Niga you've been hacked > "%desktop%\Youvebeenhacked6.txt"
+for /L %%i in (1,1,1000) do echo Dumb you've been crashed>>"%desktop%\\Youvebeenhacked1.txt"
+echo Dumb you've been crashed > "%desktop%\Youvebeenhacked2.txt"
+echo Dumb you've been crashed > "%desktop%\Youvebeenhacked3.txt"
+echo Dumb you've been crashed > "%desktop%\Youvebeenhacked4.txt"
+echo Dumb you've been crashed > "%desktop%\Youvebeenhacked5.txt"
+echo Dumb you've been crashed > "%desktop%\Youvebeenhacked6.txt"
 
 echo.
 echo You've been succesfully fucked by P4TEU!
